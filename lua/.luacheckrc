3d0b3156399ebfc3b2191569606f8924e3dd7f7b
xiongjiabin <xjb@lamudatech.com>
2021-04-14 22:09:37 +0800

    added modbus crc16

---
+++ lua/.luacheckrc
@@ -0,0 +1,4 @@
+stds.ngx = {
+    globals = {"ngx","self","table","math","string"}
+}
+std = "min+ngx"
---
+++ lua/test/testcrc16.lua
@@ -0,0 +1,7 @@
+local crc = require 'lib.crc16'
+
+local a1 = crc({0x1,0x2,0x3,0x4})
+ngx.say(string.format("pi = %x,%x", a1[1],a1[2]))
+
+local a2 = crc({0x0F,0x06, 0x00, 0x12, 0x00, 0x00})
+ngx.say(string.format("pi = %x,%x", a2[1],a2[2]))
